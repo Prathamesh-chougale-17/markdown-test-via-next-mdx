0eb06aa588a4c312cf93d8abf9bb3c460e338381
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-06-06 16:17:34 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import Example from "./example.mdx";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { Code } from "./code";
 
 export default function Home() {
   return (
     <div>
       <h1>Next.js + MDX</h1>
-      <MDXRemote
+      {/* <MDXRemote
         source={`# Hello World
 
       This is from Server Components!
       `}
       />
-      <Example />
+      <Example /> */}
+      <Code code="`const numbers = [1, 2, 3]{:js}`" />
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
